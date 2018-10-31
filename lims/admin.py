6b084d8bf550022d55da605356d59b84a8d437c9
Zach Smith <z4smith@ucsd.edu>
2018-10-31 15:51:26 +0000

    Adds Specimen and Specimen Type to admin panel

--- lims/admin.py
+++ lims/admin.py
@@ -3,3 +3,5 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.PatientModel)
+admin.site.register(models.SpecimenModel)
+admin.site.register(models.SpecimenType)
