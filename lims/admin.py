0fd8adc98960d1d69cb7ba8c0b8153d03a9ef992
Zach Smith <z4smith@ucsd.edu>
2018-11-01 21:20:34 +0000

    Adds aliquot and aliquot type and visit to admin panel

--- lims/admin.py
+++ lims/admin.py
@@ -5,3 +5,6 @@ from . import models
 admin.site.register(models.PatientModel)
 admin.site.register(models.SpecimenModel)
 admin.site.register(models.SpecimenType)
+admin.site.register(models.AliquotModel)
+admin.site.register(models.AliquotType)
+admin.site.register(models.VisitModel)
