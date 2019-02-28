aac1dedc53bafc1a04463d8a9c6e791b8bb8564e
Zach Smith <z4smith@ucsd.edu>
2019-02-28 21:03:31 +0000

    remove abstract user until custom user further explored

--- lims/models.py
+++ lims/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 from datetime import datetime
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    pass
-
 
 class BoxModel(models.Model):
     name = models.CharField(max_length=50)
