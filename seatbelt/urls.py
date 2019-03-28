d17538611f4cb145e67d46d174d5dffaf35090a9
Zach Smith <z4smith@ucsd.edu>
2019-03-28 14:17:26 -0700

    adds jwt cookie for auth and exposed csrf token for security

--- seatbelt/urls.py
+++ seatbelt/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from . import views
+
 
 from graphene_django.views import GraphQLView
 from graphql_jwt.decorators import jwt_cookie
@@ -9,6 +11,7 @@ from graphql_jwt.decorators import jwt_cookie
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', jwt_cookie(GraphQLView.as_view(graphiql=True))),
+    path('csrf/', views.csrf),
 ]
 
 if settings.DEBUG:
