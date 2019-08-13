556f368f454c7f81b50cc1082c4522e95ebcb2b3
Zach Smith <z4smith@ucsd.edu>
2019-08-13 10:20:19 -0700

    Add courier, shipment, destination to models

--- lims/models.py
+++ lims/models.py
@@ -51,6 +51,35 @@ class ScheduleModel(models.Model):
     def __str__(self):
         return self.name
 
+class ShipmentModel(models.Model):
+    contents = models.ManyToManyField('AliquotModel')
+    courier = models.ForeignKey('CourierModel',
+                                on_delete=models.SET_NULL,
+                                blank=True,
+                                null=True)
+    courier_number = models.CharField(max_length=255, blank=True, null=True)
+    # TODO What should we do if a destination is removed?
+    destination = models.ForeignKey('DestinationModel',
+                                    on_delete=models.SET_NULL,
+                                    blank=True,
+                                    null=True)
+    sent_date = models.DateTimeField(blank=True, null=True)
+    received_date = models.DateTimeField(blank=True, null=True)
+    notes = models.CharField(max_length=255, blank=True, null=True)
+
+class DestinationModel(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class CourierModel(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 # Storage related models
 class BoxSlotModel(models.Model):
     """
